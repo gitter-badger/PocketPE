b4a6c9b986935ebc317f5e56c40fee39e3b74209
Dunx <mccleanduncan03@hotmail.com>
2015-04-09 13:51:45 +0100

    Create setup.cmd

---
+++ setup.cmd
@@ -0,0 +1,17 @@
+POCKET PE SERVER SOFTWARE FOR MINECRAFT POCKET EDITION
+                    -------------------
+                    |      Setup      |
+                    -------------------
+
+1. What would you like to name your server?
+<name>
+2. What gamemode should this server run on for Default? Please DONT use numbers
+gamemode <creative,survival,adventure,spectator>
+3. What is your Access Address:
+<access_address>
+4. What is your Access Port?
+<access_port>
+5. In-Game Language (en - english)
+<lang>
+6. Type 'y' to Start your server
+<y>
